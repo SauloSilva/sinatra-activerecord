b0f6e14b9cb39d4b388341fc2f45e4d86606c958
saulosantiago <saulodasilvasantiago@gmail.com>
2015-01-19 14:00:52 -0200

    [issue_14] fixing connection error on rake db:seed

--- lib/sinatra/activerecord/rake.rb
+++ lib/sinatra/activerecord/rake.rb
@@ -2,10 +2,13 @@ require 'active_record'
 require 'fileutils'
 
 namespace :db do
+  task seed: :load_config do
+  end
+
   desc "migrate your database"
   task :migrate do
     ActiveRecord::Migrator.migrate(
-      'db/migrate', 
+      'db/migrate',
       ENV["VERSION"] ? ENV["VERSION"].to_i : nil
     )
   end
@@ -16,7 +19,7 @@ namespace :db do
     abort("no NAME specified. use `rake db:create_migration NAME=create_users`") if !name
 
     migrations_dir = File.join("db", "migrate")
-    version = ENV["VERSION"] || Time.now.utc.strftime("%Y%m%d%H%M%S") 
+    version = ENV["VERSION"] || Time.now.utc.strftime("%Y%m%d%H%M%S")
     filename = "#{version}_#{name}.rb"
     migration_name = name.gsub(/_(.)/) { $1.upcase }.gsub(/^(.)/) { $1.upcase }
 
